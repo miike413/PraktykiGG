0fcbafc24efb85fa8d71649a4e586a649c27a553
miike413 <shpero1337@gmail.com>
2021-02-23 14:35:25 +0100

    zadanie 1,2,3,4

---
+++ wszystkie zadanka/src/com/company/Main.java
@@ -0,0 +1,68 @@
+
+zadanie 3
+public class Main {
+
+
+
+    public class Main {
+
+        public static void main(String[] args) {
+            double sqrt = Math.sqrt(3);
+            double A = 7;
+
+
+            System.out.println("Pole Trojkąta równobocznego o boku 7cm wynośi:" + (A*A)* sqrt/ 4);
+        }
+    }
+
+
+
+}
+}
+
+zadanie 1
+        package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1;
+        double d = 3;
+        char c = 'f';
+        String S;
+        System.out.println();
+
+    }
+
+
+}
+
+zadanie 2
+        package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum;
+        int myIntA= 20;
+        int myIntB = 7;
+        sum = myIntA*myIntA + myIntB*myIntB;
+        System.out.println("a^2+b^2=" +sum);
+
+
+    }
+}
+
+zadanie 4
+        package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myStringA = "Michał";
+        String myStringB = "Radzicki";
+        // write your code here
+        System.out.println(myStringA +"\t"+ myStringB);
+    }
+}
+
